d984cb31b3b49ce56e21afaf00e05393a951217d
20eva <yefa.attia@esprit.tn>
2024-05-02 11:49:46 +0100

    notification

--- src/Controller/DossierBackController.php
+++ src/Controller/DossierBackController.php
@@ -20,20 +20,38 @@ class DossierBackController extends AbstractController
     {
         // Fetch all dossiers from the database
         $allDossiers = $dossierRepository->findAll();
+        $produitsEnRupture = $dossierRepository->findProduitsEnRupture();
+
+        $nom = $request->query->get('Nom');
+       // $tri = $request->query->get('tri', 'nom');
+        /*if ($nom) {
+            $allDossiers = $dossierRepository->searchAndSort($nom, $tri);
+        } else {
+            $allDossiers = $dossierRepository->findAllSorted($tri);
+        }*/
+        if (!empty($produitsEnRupture)) {
+            $message = '';
+            foreach ($produitsEnRupture as $produitEnRupture) {
+                $message .= sprintf('%s a payé plus que 5$ ', $produitEnRupture->getNom());
+            }
+            $this->addFlash('warning', $message);
+        }
+    
 
         // Paginate the dossiers using KnpPaginatorBundle
         $dossiers = $paginator->paginate(
             $allDossiers, // Data to paginate
             $request->query->getInt('page', 1), // Page number, default is 1
-            5// Number of items per page
+            4// Number of items per page
         );
 
         return $this->render('dossier/indexBack.html.twig', [
             'dossiers' => $dossiers,
+            'produitsEnRupture' => $produitsEnRupture,
         ]);
     }
 
-
+ 
     
     #[Route('/{id}/delete', name: 'app_dossier_back_delete', methods: ['GET'])]
     public function delete(Request $request, Dossier $dossier, EntityManagerInterface $entityManager): Response
@@ -54,17 +72,7 @@ class DossierBackController extends AbstractController
     }
 
 
-    
-    #[Route('/cin', name:'dossiers_cin')]
-     
-    public function dossiersByEtat(DossierRepository $DossierRepository): Response
-    {
-        $dossiers = $DossierRepository->findBycin();
-        
-        return $this->render('dossier/indexBack.html.twig', [
-            'dossiers' => $dossiers,
-        ]);
-    }
+  
     
     #[Route('/montant', name:'dossiers_montant')]
     
@@ -76,6 +84,31 @@ class DossierBackController extends AbstractController
             'dossiers' => $dossiers,
         ]);
     }
+    #[Route('/', name:'dossiers_cin')]
+    
+    public function dossiersByCin(DossierRepository $DossierRepository): Response
+    {
+        $dossiers = $DossierRepository->findByCin();
+        
+        return $this->render('dossier/indexBack.html.twig', [
+            'dossiers' => $dossiers,
+        ]);
+    }
 
+  
+
+    #[Route('/search', name: 'dossier_search')] // Define the route and provide a unique name
+    public function searchByCin(DossierRepository $DossierRepository, Request $request): Response
+    {
+        $cin = $request->query->get('cin');
+    
+        // Call the repository method to search for dossier by CIN
+        $dossier = $dossierRepository->searchByCin($cin);
+    
+        return $this->render('dossier/indexBack.html.twig', [
+            'dossier' => $dossier,
+        ]);
+    }
+    
 
 }
