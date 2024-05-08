56ea875489c6a602624fb51bd3d81cab4f6f2669
Raouf2022 <abderraouf.mahmoudi@esprit.tn>
2024-05-08 17:08:50 +0100

    user merge blog

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -89,8 +89,8 @@ public function deleteUser(int $idu, EntityManagerInterface $entityManager): Res
 
 private function sendWelcomeSms($phoneNumber)
     {
-        $sid = ''; // Replace with your Twilio account SID
-        $token = ''; // Replace with your Twilio auth token
+        $sid = '';
+        $token = '';
         $twilio = new Client($sid, $token);
         $message = 'Bienvenu dans Karhabte\nTon compte a été crée avec succés.';
 
